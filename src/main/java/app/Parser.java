9d392a46c3bf9263cd3c778a0ba124c54bbdfe93
Fabrice Jeannet <fabricejeannet@coolworking.fr>
2016-07-06 08:12:16 +0200

    Modification of the options -i can now be a file or a directory of file -w tells the app it must write the parsing as a json file

--- src/main/java/app/Parser.java
+++ src/main/java/app/Parser.java
@@ -1,5 +1,6 @@
 package app;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import domain.parser.BellePouleParser;
 import domain.parser.EnGardeParser;
 import domain.parser.SportCloudParser;
@@ -9,7 +10,9 @@ import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
 
 import java.io.*;
+import java.util.HashMap;
 import java.util.Optional;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -28,62 +31,118 @@ public class Parser {
 
         cmdLineParser.parseArgument(args);
 
-        String json = "";
-        SportCloudParser parser;
-        Optional<String> encoding = null;
+        handleInputFiles();
 
-        if(inputFile !=null) {
+        if(mustWriteJson) writeOutputFile();
 
-            logger.info("Parsing " + inputFile.getName());
+    }
 
-
-            String filename = inputFile.getName();
-            String extension = filename.substring(filename.lastIndexOf('.'), filename.length());
-
-
-           if(extension.equals(".xml")) {
-                logger.info(inputFile.getName() + " is from 'En Garde'");
-                parser = EnGardeParser.create();
-            } else {
-                logger.info(inputFile.getName() + " is from 'Belle Poule'");
-                parser = BellePouleParser.create();
-            }
-
-            parser.parse(new FileReader(inputFile));
-
-            json = parser.getJson();
-            encoding.ofNullable(parser.getEncoding());
-            logger.info("Encoding : " + encoding);
-
-            logger.info(json);
-
-        }
-
-        if(outputFile != null) {
+    private void writeOutputFile() throws IOException {
+        for (String filename : mapFilenameJson.keySet()) {
+            File outputFile = new File(getFilenameWithJsonExtension(filename));
             logger.info("Writing " + outputFile.getName());
-
-
-/*
             Writer fw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outputFile), encoding.orElse("UTF-8")));
             fw.write(json);
             fw.close();
-*/
+        }
+    }
+
+    private String getFilenameWithJsonExtension(String filename){
+        int lastDotIndex = filename.lastIndexOf('.');
+        return filename.substring(0,lastDotIndex).concat(".json");
+    }
+
+    private void handleInputFiles() throws JsonProcessingException, FileNotFoundException {
+
+        if(inputFile == null) {
+            logger.info("Input file is missing.");
+            System.exit(1);
+        }
+
+        else if(inputFile.isDirectory()) {
+            logger.info("Browsing folder");
+            File[] files =  inputFile.listFiles(new FileFilter() {
+                public boolean accept(File pathname) {
+                    String extension = getFileExtension(pathname.getName());
+                    return extension.equalsIgnoreCase(".xml");
+                }
+            });
+            logger.info(files.length + " xml files found");
 
 
-            FileWriter fw = new FileWriter(outputFile);
+            for (File file : files) {
+                handleThisFile(file);
+            }
+        }
 
-            fw.write(json);
-            fw.flush();
-            fw.close();
+        else {
+            handleThisFile(inputFile);
         }
 
     }
 
+
+    private void handleThisFile(File file){
+        try {
+            String json = parse(file);
+            mapFilenameJson.put(file.getName(), json);
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "File " + file.getName() + " aborted.");
+            logger.log(Level.SEVERE, e.getMessage());
+        }
+    }
+
+    private String parse(File file) throws FileNotFoundException, JsonProcessingException, DocumentException {
+
+        logger.info("Parsing " + file.getName());
+
+        SportCloudParser parser;
+
+        String filename = file.getName();
+
+        String extension = getFileExtension(filename);
+
+
+        if(extension.equals(".xml")) {
+            logger.info(file.getName() + " is from 'En Garde'");
+            parser = EnGardeParser.create();
+        } else {
+            logger.info(file.getName() + " is from 'Belle Poule'");
+            parser = BellePouleParser.create();
+        }
+
+        parser.parse(new FileReader(file));
+
+        json = parser.getJson();
+
+        encoding = Optional.ofNullable(parser.getEncoding());
+        logger.info("Encoding : " + encoding);
+
+        logger.info(json);
+
+        return json;
+    }
+
+    private String getFileExtension(String filename){
+        return filename.substring(filename.lastIndexOf('.'), filename.length());
+    }
+
+    private HashMap<String, String> mapFilenameJson = new HashMap<String, String>();
+
+    Optional<String> encoding;
+
+    private String json = new String();
+
     @Option(name="-i",usage="Input file",metaVar="INPUT")
     private File inputFile = null;
 
-    @Option(name="-o",usage="Output file",metaVar="OUPTUT")
-    private File outputFile = null;
+    @Option(name="-w",usage="Writes parsed json into a file .json")
+    private Boolean mustWriteJson = false;
+
+    @Option(name="-s",usage="Sends the json to the given API via HTTP PUT")
+    private String apiUrl;
+
+
 
     private Logger logger = Logger.getLogger("SportCloud -> ");
 
